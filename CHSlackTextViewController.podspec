cc133a4ef71ad632f490a438713149685a7c6e5e
intoxicated <yakle1218@gmail.com>
2017-12-13 16:38:57 +0900

    Fixed layout priority

--- CHSlackTextViewController.podspec
+++ CHSlackTextViewController.podspec
@@ -1,4 +1,4 @@
-@version = "1.10.14"
+@version = "1.10.15"
 
 Pod::Spec.new do |s|
   s.name         		= "CHSlackTextViewController"
--- Source/SLKTextInputbar.m
+++ Source/SLKTextInputbar.m
@@ -678,7 +678,7 @@ NSString * const SLKTextInputbarDidMoveNotification =   @"SLKTextInputbarDidMove
                               @"right" : @(self.contentInset.right),
                               };
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(left)-[leftButton(0)][textView]-(right)-[rightButton(60)]-(right)-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(left)-[leftButton(0)][textView]-(right@750)-[rightButton(60)]-(right)-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[leftButton(0)]-(0@750)-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[rightButton]-(<=0)-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(left@250)-[charCountLabel(<=50@1000)]-(right@750)-|" options:0 metrics:metrics views:views]];
